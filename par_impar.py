ead545b63b19500c3ce28912d5dd0f48ae0180eb
TAINA RAMOS <taina.ramos@senacsp.edu.br>
2025-08-20 11:20:03 -0300

    Int: Iniciando o estudo Python

---
+++ par_impar.py
@@ -0,0 +1,9 @@
+# Declarar a variável que irá guardar um número
+valor = 12
+if(valor & 2 == 0):
+    print("Este valor é par")
+
+else:
+    print("Este valor é impar")
+
+
---
+++ pontoevirgula.py
@@ -0,0 +1,4 @@
+# Declaração de variáveis com conteúdo e separação utilizando
+# ponto e virgula
+pais = "Brasil" ; uf = "SP" ; cidade = "Suzano" ; espaco = " - "
+print(pais + espaco + uf + espaco + cidade)
